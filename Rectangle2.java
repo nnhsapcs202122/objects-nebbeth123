b2174a81c884b5c89638de4897387a6d779b17ea
nebbeth123 <71365143+nebbeth123@users.noreply.github.com>
2021-09-09 12:30:21 -0500

    Update Rectangle2.java

--- Rectangle2.java
+++ Rectangle2.java
@@ -5,6 +5,7 @@ public class Rectangle2
     public static void main(String args[])
     {
         Rectangle r1 = new Rectangle(0,0,10,10);
-        
+        System.out.println(r1);
+        Rectangle r1.add(1,1,10,10);
     }
 }
